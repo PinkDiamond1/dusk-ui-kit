d84bd4eb0ca05fb2b2c28dbc1baf5cde28622b1e
Alex Panturu <panturu.alexandru@gmail.com>
2022-04-12 17:07:04 +0100

    :sparkles:  Storybook UI Kit Gauge story

---
+++ storybook/stories/atoms/gauge/Gauge.stories.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { Meta, Story } from "@storybook/addon-svelte-csf";
+  // import results from "../../../../.jest-test-results.json";
+  // import { withTests } from "@storybook/addon-jest";
+  import Gauge from "@dusk-network/gauge";
+  import meta from "../../meta.js";
+
+  let minValue = 0;
+  let maxValue = 20;
+  let value = 6;
+</script>
+
+<Meta
+  title="Components/Atoms/Gauge"
+  parameters="{{
+    layout: 'centered',
+    docs: {
+      source: {
+        type: 'code',
+      },
+    },
+    jest: ['packages/atoms/gauge/Gauge.test.js'],
+  }}"
+  component="{Gauge}"
+  argTypes="{meta('atoms/gauge/Gauge', {})}"
+/>
+
+<Story
+  name="Default gauge"
+  args="{{
+    minValue: minValue,
+    maxValue: maxValue,
+    value: value,
+  }}"
+  let:args
+>
+  <Gauge {...args} />
+</Story>
