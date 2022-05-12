d5114308e05cb15ece58b2d40c63b82ed7fa769a
Alex Panturu <panturu.alexandru@gmail.com>
2022-05-12 18:28:46 +0300

    :pencil:  update UI Kit DropDown example

--- examples/src/atoms/drop-down/DropDown_01.svelte
+++ examples/src/atoms/drop-down/DropDown_01.svelte
@@ -1,7 +1,13 @@
 <script>
   import DropDown from "@dusk-network/drop-down";
 
-  const options = ["option 1", "option 2", "option 3", "option 4", "option 5"];
+  const options = [
+    { name: "option 1" },
+    { name: "option 2" },
+    { name: "option 3" },
+    { name: "option 4" },
+    { name: "option 5" },
+  ];
   function onSelect() {
     //Triggered on new selected value
   }
